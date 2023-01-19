8e0eb028ca95a5cad5c7ebd9930419048870591e
Fiona Brozas <brozasfiona16@gmail.com>
2023-01-19 19:17:39 +0800

    - Updated interaction to raycast; Added proximity prompt & trigger

--- Assets/Script/INVENTORY/InventoryManager.cs
+++ Assets/Script/INVENTORY/InventoryManager.cs
@@ -32,8 +32,8 @@ public class InventoryManager : MonoBehaviour
         foreach (var item in Items)
         {
             GameObject obj = Instantiate(InventoryItem, ItemContent);
-            var itemName = obj.transform.Find("itemName").GetComponent<Text>();
-            var itemIcon = obj.transform.Find("icon").GetComponent<Image>();
+            Text itemName = obj.transform.Find("ItemName").GetComponent<Text>();
+            Image itemIcon = obj.transform.Find("ItemIcon").GetComponent<Image>();
 
             itemName.text = item.itemName;
             itemIcon.sprite = item.icon;
