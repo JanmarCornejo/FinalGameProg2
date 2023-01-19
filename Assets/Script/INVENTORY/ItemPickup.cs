153f184a3284e7f0924d6ea05db3c166a82743ef
Fiona Brozas <brozasfiona16@gmail.com>
2023-01-19 21:44:42 +0800

    - Added more popup texts (instructions, obtaining keycard prompts)

--- Assets/Script/INVENTORY/ItemPickup.cs
+++ Assets/Script/INVENTORY/ItemPickup.cs
@@ -1,35 +1,100 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Events;
+using UnityEngine.UI;
+using TMPro;
 
 public class ItemPickup : MonoBehaviour
 {
+    [Header("Item Type")]
+    [SerializeField] private bool isFirstKeyCard = true;
+    [SerializeField] private bool isSecondKeyCard = true;
+    [SerializeField] private bool isThirdKeyCard = true;
+
+    public TextMeshProUGUI showAcquiredPrompt;
+
     public Item item;
 
     public Camera playerCamera;
 
+    private float timeToAppear = 2f;
+    private float timeWhenDisappear;
+
     void Update()
     {
-        InteractItem();
+        if (isFirstKeyCard)
+            InteractFirstKeyCard();
+
+        if (isSecondKeyCard)
+            InteractSecondKeyCard();
+
+        if (isThirdKeyCard)
+            InteractThirdKeyCard();
+
+        if (showAcquiredPrompt.enabled && (Time.time >= timeWhenDisappear))
+        {
+            showAcquiredPrompt.enabled = false;
+        }
     }
 
     void Pickup()
     {
         InventoryManager.Instance.Add(item);
         Destroy(gameObject);
-        Debug.Log("Item Obtained.");
     }
 
-    private void InteractItem()
+    public void EnableText()
+    {
+        showAcquiredPrompt.enabled = true;
+        timeWhenDisappear = Time.time + timeToAppear;
+    }
+
+    private void InteractFirstKeyCard()
     {
         if (Physics.Raycast(playerCamera.transform.position, playerCamera.transform.forward, out RaycastHit hit, 3))
         {
-            if (hit.transform.CompareTag("Item"))
+            if (hit.transform.CompareTag("FirstKeyCard"))
             {
                 if (Input.GetKey(KeyCode.E))
                 {
                     Pickup();
+                    EnableText();
+
+                    Debug.Log("Level 2 KeyCard Obtained.");
+                }
+            }
+        }
+    }
+
+    private void InteractSecondKeyCard()
+    {
+        if (Physics.Raycast(playerCamera.transform.position, playerCamera.transform.forward, out RaycastHit hit, 3))
+        {
+            if (hit.transform.CompareTag("SecondKeyCard"))
+            {
+                if (Input.GetKey(KeyCode.E))
+                {
+                    Pickup();
+                    EnableText();
+
+                    Debug.Log("CEO KeyCard Obtained.");
+                }
+            }
+        }
+    }
+
+    private void InteractThirdKeyCard()
+    {
+        if (Physics.Raycast(playerCamera.transform.position, playerCamera.transform.forward, out RaycastHit hit, 3))
+        {
+            if (hit.transform.CompareTag("ThirdKeyCard"))
+            {
+                if (Input.GetKey(KeyCode.E))
+                {
+                    Pickup();
+                    EnableText();
+
+                    Debug.Log("Executive Card Obtained.");
                 }
             }
         }
