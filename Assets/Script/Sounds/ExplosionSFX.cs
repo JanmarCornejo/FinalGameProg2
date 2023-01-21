4df5454129f5aaf402e2015b358e14103743f2b0
Fiona Brozas <brozasfiona16@gmail.com>
2023-01-21 18:32:15 +0800

    - Fixed exposion sfx trigger

--- Assets/Script/Sounds/ExplosionSFX.cs
+++ Assets/Script/Sounds/ExplosionSFX.cs
@@ -4,13 +4,17 @@ using UnityEngine;
 
 public class ExplosionSFX : MonoBehaviour
 {
+    public GameObject explosionSoundTrigger;
+
     void OnTriggerEnter(Collider EnteringTheTrigger)
     {
         if (EnteringTheTrigger.tag == "Player")
         {
             FindObjectOfType<SoundManager>().Play("Explosion");
             Debug.Log("Explosion SFX.");
-        
+            explosionSoundTrigger.gameObject.SetActive(false);
+
+
         }
     }
 }
